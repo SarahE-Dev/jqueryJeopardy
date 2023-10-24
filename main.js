6484c9c1e546bab0272f413728529bce7a48e386
Sarah E <seatherly.prsvr@gmail.com>
2023-10-24 14:49:37 -0500

    edit

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-let containerHTML = $(`<div class="container text-center" id="grid"></div>`)
+// let containerHTML = $(`<div class="container text-center" id="grid"></div>`)
 
-$('body').append(containerHTML)
+// $('body').append(containerHTML)
 
 // for(let i=1; i < 6; i++){
 //     $('#grid').append(`<div class="row" id="row${i}">th</div>`)
 // }
 
-for(let i=1; i < 6; i++){
-    // for(let j=0; j <6; i++){
-    // $(`#row${i}`).append(`<div class="col-3" id="col${j}">Col ${j}</div>`)
-    // }
-}
+console.log('hello');
+
+// for(let i=1; i < 6; i++){
+//     for(let j=0; j <6; i++){
+//     $(`#row${i}`).append(`<div class="col-3" id="col${j}">Col ${j}</div>`)
+//     }
+// }
