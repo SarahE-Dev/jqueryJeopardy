5f74b10732416c5b5ad84846fa79db0b190bf06a
Sarah E <seatherly.prsvr@gmail.com>
2023-10-30 09:09:04 -0500

    event listener edit

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ for(let i=1; i < 6; i++){
   }
   gameBoard.append(row)
 }
+
 $('#row1 .col').text('$100')
 $('#row2 .col').text('$200')
 $('#row3 .col').text('$400')
@@ -29,40 +30,38 @@ $('.container').prepend(header)
 let actualQuestion;
 let pointsEarned;
 let totalScore = 0;
+let pointsYouHave = 0;
 
 function setItems(){
-if('total' in localStorage){
-  $('#score').text(localStorage.getItem('total'))
-}else{
-  $('#score').text(0)
-}
-
-for(let i=0; i<=24; i++){
-  if(`square${i}` in localStorage){
-    $(`#square${i}`).addClass('active')
+  if('total' in localStorage){
+    $('#score').text(localStorage.getItem('total'))
   }else{
-    $(`#square${i}`).removeClass('active')
+    $('#score').text(0)
+    }
+
+  for(let i=0; i<=24; i++){
+    if(`square${i}` in localStorage){
+      $(`#square${i}`).addClass('active')
+    }else{
+      $(`#square${i}`).removeClass('active')
+    }
   }
 }
-}
-setItems()
 
+setItems()
 
 $('.col').on('click', (event)=>{
   $(event.target).addClass('active');
-  console.log(event.target.id);
   localStorage.setItem(event.target.id, true)
   fetch('jeopardy.json')
     .then(data=>data.json())
     .then(resp=>{
       let answer$ = $(event.target).text()
-      console.log(answer$)
       let answerArr = resp.filter((item)=>{
         if(item['value'] === answer$){
           return item
         }
       })
-      console.log(answerArr);
       let questionsLength = answerArr.length;
       let questionPick = Math.floor(Math.random() * questionsLength) + 1;
       actualQuestion = answerArr[questionPick]
@@ -70,31 +69,35 @@ $('.col').on('click', (event)=>{
       $('#question').text('')
       $('#question').append(actualQuestion.question)
       });
-      $('#submit-answer').on('click', ()=>{
-        let answerToQuestion = $('#answer').val();
-        let pointsYouHave = $('#score').text()
-        if(answerToQuestion == actualQuestion.answer){
-          pointsEarned = actualQuestion.value;
-          pointsEarned = pointsEarned.slice(1);
-          totalScore = parseInt(pointsYouHave) + parseInt(pointsEarned);
-          
-          console.log(totalScore);
-          console.log(pointsEarned);
-          console.log(pointsYouHave);
-          $('#answer').val('');
-          $('#question').text('Your answer is correct!')
-          }
-        localStorage.setItem('total', totalScore)
-        $('#score').text(localStorage.getItem('total'));
-      })
-      
-      
     })
 
-    $('#clear-score').on('click', ()=>{
-      localStorage.clear()
-      setItems()
-    })
+$('#clear-score').on('click', ()=>{
+  localStorage.clear()
+  setItems()
+  pointsYouHave = 0;
+  actualQuestion = '';
+})
+
+$('#submit-answer').on('click', (e)=>{
+    let answerToQuestion = $('#answer').val();
+    if(actualQuestion.answer.toLowerCase() == answerToQuestion.toLowerCase()){
+        pointsYouHave = $('#score').text()
+        pointsEarned = actualQuestion.value;
+        pointsEarned = pointsEarned.slice(1);
+        totalScore = parseInt(pointsYouHave) + parseInt(pointsEarned);
+        $('#answer').val('');
+        $('#question').text('Your answer is correct!')
+    }else{
+      $('#question').text('That is not correct, please pick a new square.')
+    }
+    
+    localStorage.setItem('total', totalScore)
+    $('#score').text(localStorage.getItem('total'));
+    actualQuestion = '';
+  })
+
+
+
 
   
 
